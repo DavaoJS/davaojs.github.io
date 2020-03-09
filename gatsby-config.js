d3dc104865f3fc8c74bfdba434e4876f2f1442a4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-03-09 11:52:57 +0800

    Update gatsby-config settings to match current project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Davao JS`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Davao JS Core Team`,
+      summary: `A small team of developers under Davao Javascript (JS) Community`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://davaojs.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      facebook: `davaojs`,
+      medium: `davao-js`,
+      twitter: `davaojs`,
+
     },
   },
   plugins: [
@@ -60,8 +63,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `DavaoJS Official Website`,
+        short_name: `DavaoJS`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
